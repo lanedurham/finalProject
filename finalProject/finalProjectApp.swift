b14c78b5650342f68d41a652f333bce6b1fa4bca
Lane Durham <wlaned@unc.edu>
2026-04-20 17:53:52 -0400

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Lane Durham on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
